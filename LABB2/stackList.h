c03cc0d8f18cf89bcf09acf00940e084b331a9e1
DevRanjo <rania.leontiou@outlook.com>
2026-04-10 15:31:53 +0200

    Added stack lists

--- LABB2/stackList.h
+++ LABB2/stackList.h
@@ -10,4 +10,10 @@ typedef struct stackTop{
     Node* top;
 }Stack;
 
+Stack* createStack();
+void push(Stack* stack, int value);
+int pop(Stack* stack);
+int isEmpty(Stack* stack);
+void freeStack(Stack* stack);
+
 #endif
