bab835dae7ca682153300c959f28adb92d1e68a2
DevRanjo <rania.leontiou@outlook.com>
2026-04-10 14:36:46 +0200

    addad the last queue array functions

--- LABB2/queueArray.c
+++ LABB2/queueArray.c
@@ -23,9 +23,21 @@ bool isEmpty(Queue* q){
 }
 
 void enqueue(Queue* q, int data){
-    if((q->head+1) % SIZE == q->head){ //if 19+1/20 gives r=0 <- head index
-        printf("Queue is empty\n");
+    if((q->head+1) % SIZE == q->head){ //if ex 19+1/20 gives r=0 <- head index
+        printf("Queue is full\n");
         return;
     }
-    q->ta
+    q->queue[q->tail] = data; //add data at the last index of queue
+    q->tail = (q->tail+1) % SIZE; //update tail index. Wraps around if there is array overflow 
+}
+
+bool dequeue(Queue* q){
+    if(isEmpty(q)){
+        printf("Queue is empty\n");
+        return false;
+    }
+    int data = q->queue[q->head]; //save the value of the first element in queue
+    q->head = (q->head+1) % SIZE; //move head pointer to next index (next element in queue)
+    //if overflow->wrap arount 
+    return data;
 }
